7163cbde8d6983e6eaf87aa3024e561538f5051b
Samuel Gratzl <sam@sgratzl.com>
2019-11-19 14:02:14 -0500

    cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,40 @@
 <template>
-  <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+  <v-app id="inspire">
+    <v-app-bar app clipped-right dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>UPenn Contrast</v-toolbar-title>
+      <v-spacer />
     </v-app-bar>
 
+    <v-navigation-drawer v-model="drawer" app>
+      <Menu />
+    </v-navigation-drawer>
+
     <v-content>
-      <HelloWorld />
+      <router-view />
     </v-content>
+
+    <v-footer app>
+      <span>Kitware</span>
+      <v-spacer />
+      <span>&copy; 2019</span>
+    </v-footer>
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import Menu from "./components/Menu.vue";
 
 export default Vue.extend({
   name: "App",
 
   components: {
-    HelloWorld
+    Menu
   },
 
   data: () => ({
-    //
+    drawer: false
   })
 });
 </script>
---
+++ src/components/Menu.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-list dense>
+    <v-list-item to="/">
+      <v-list-item-action>
+        <v-icon>mdi-exit-to-app</v-icon>
+      </v-list-item-action>
+      <v-list-item-content>
+        <v-list-item-title>Home</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+    <v-list-item to="/about">
+      <v-list-item-action>
+        <v-icon>mdi-exit-to-app</v-icon>
+      </v-list-item-action>
+      <v-list-item-content>
+        <v-list-item-title>About</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+  </v-list>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({});
+</script>
