249f8396e22c928de37d480095cdadd565f825cb
Samuel Gratzl <sam@sgratzl.com>
2019-11-25 14:20:15 -0500

    add menu entry

--- src/layout/Menu.vue
+++ src/layout/Menu.vue
@@ -8,18 +8,30 @@
         <v-list-item-title>Home</v-list-item-title>
       </v-list-item-content>
     </v-list-item>
-    <v-list-item to="/about">
+    <v-list-item :to="{ name: 'newdataset' }">
       <v-list-item-action>
-        <v-icon>mdi-exit-to-app</v-icon>
+        <v-icon>mdi-plus-circle</v-icon>
       </v-list-item-action>
       <v-list-item-content>
-        <v-list-item-title>About</v-list-item-title>
+        <v-list-item-title>New Dataset</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+    <v-list-item :to="{ name: 'newconfiguration' }" v-if="store.dataset">
+      <v-list-item-action>
+        <v-icon>mdi-plus-circle</v-icon>
+      </v-list-item-action>
+      <v-list-item-content>
+        <v-list-item-title>New Configuration</v-list-item-title>
       </v-list-item-content>
     </v-list-item>
   </v-list>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-export default Vue.extend({});
+import { Vue, Component } from "vue-property-decorator";
+import store from "@/store";
+@Component
+export default class Menu extends Vue {
+  readonly store = store;
+}
 </script>
