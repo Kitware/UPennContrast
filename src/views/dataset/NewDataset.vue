7800c3c23651588b85a352d6eaaeb00d6a6636e9
Roni Choudhury <roni.choudhury@kitware.com>
2020-03-23 11:41:40 -0400

    Add initial attempt at filename parsing

--- src/views/dataset/NewDataset.vue
+++ src/views/dataset/NewDataset.vue
@@ -98,6 +98,32 @@ function findCommonPrefix(strings: string[]): string {
   return result.join("");
 }
 
+function parseTime(s: string): string | null {
+  const re = /time(\d+)/;
+  const match = s.match(re);
+
+  return match ? match[1] : null;
+}
+
+function parseXY(s: string): string | null {
+  const re = /XY(\d+)/;
+  const match = s.match(re);
+
+  return match ? match[1] : null;
+}
+
+function collectFilenameMetadata(
+  filenames: string[]
+): { xy: string[]; t: string[] } {
+  const times = filenames.map(parseTime);
+  const xys = filenames.map(parseXY);
+
+  return {
+    t: times.filter(d => d !== null).sort(),
+    xy: xys.filter(d => d !== null).sort()
+  };
+}
+
 @Component({
   components: {
     GirderLocationChooser,
@@ -204,6 +230,10 @@ export default class NewDataset extends Vue {
 
   nextStep() {
     this.hideUploader = true;
+
+    const filenameMetadata = collectFilenameMetadata(
+      this.files.map(f => f.file.name)
+    );
 
     this.$router.push({
       name: "dataset",
