f03c73e727b0fff6dd601009f07568107de0462e
Roni Choudhury <roni.choudhury@kitware.com>
2020-05-21 14:55:36 -0400

    Rename "splay" to "unroll" in the UI

--- src/components/ViewerToolbar.vue
+++ src/components/ViewerToolbar.vue
@@ -4,19 +4,19 @@
       <value-slider v-model="xy" label="XY Value" :min="0" :max="maxXY" />
       <v-checkbox
         v-model="splayXY"
-        label="Splay by XY"
+        label="Unroll by XY"
         v-if="maxXY > 0 || splayXY"
       />
       <value-slider v-model="z" label="Z Value" :min="0" :max="maxZ" />
       <v-checkbox
         v-model="splayZ"
-        label="Splay by Z"
+        label="Unroll by Z"
         v-if="maxZ > 0 || splayZ"
       />
       <value-slider v-model="time" label="Time Value" :min="0" :max="maxTime" />
       <v-checkbox
         v-model="splayT"
-        label="Splay by T"
+        label="Unroll by T"
         v-if="maxTime > 0 || splayT"
       />
       <switch-toggle
