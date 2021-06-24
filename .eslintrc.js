c1b51f0df06fdd0b760080a5d018c15a55e3ba3d
Adrien Boucaud <adrien.bk@free.fr>
2021-06-24 20:26:56 +0200

    Make snapshot list sortable using v-data-table

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,9 @@ module.exports = {
   extends: ["plugin:vue/essential", "@vue/prettier", "@vue/typescript"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    // Required to access v-data-table scoped slots, see https://vuetifyjs.com/en/components/data-tables/#slots
+    "vue/valid-v-slot": "off"
   },
   parserOptions: {
     parser: "@typescript-eslint/parser"
