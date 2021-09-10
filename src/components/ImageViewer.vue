45dd630c1dcc450bca729fcdfbc0199a8ce1dc3e
David Manthey <david.manthey@kitware.com>
2021-09-10 08:24:36 -0400

    Minor improvements.

--- src/components/ImageViewer.vue
+++ src/components/ImageViewer.vue
@@ -17,7 +17,7 @@
         style="width: 200px"
       />
     </div>
-    <svg xmlns="http://www.w3.org/2000/svg">
+    <svg xmlns="http://www.w3.org/2000/svg" width="0" height="0" style="position: absolute; top: -1px; left: -1px">
       <defs>
         <filter
           :id="'recolor-' + index"
@@ -398,8 +398,10 @@ export default class ImageViewer extends Vue {
       };
     }
     this.ready.layers.splice(this.layerStackImages.length);
-    this.annotationLayer.moveToTop();
-    this.uiLayer.moveToTop();
+    if (this.annotationLayer.zIndex() !== this.layerStackImages.length * 2 || this.uiLayer.zIndex() !== this.layerStackImages.length * 2 + 1) {
+      this.annotationLayer.moveToTop();
+      this.uiLayer.moveToTop();
+    }
     // set tile urls
     this.layerStackImages.forEach(
       (
