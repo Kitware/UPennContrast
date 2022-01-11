52c9fd366e7ac4b18c22f8ce78a6667491e334e7
Adrien Boucaud <adrien.bk@free.fr>
2022-01-11 16:24:42 +0100

    Fix connection removal API endpoint

--- devops/girder/plugins/AnnotationPlugin/upenncontrast_annotation/server/api/connections.py
+++ devops/girder/plugins/AnnotationPlugin/upenncontrast_annotation/server/api/connections.py
@@ -37,8 +37,8 @@ class AnnotationConnection(Resource):
                    .errorResponse('Write access was denied for the connection.', 403))
     @access.user
     @loadmodel(model='annotation_connection', plugin='upenncontrast_annotation', level=AccessType.WRITE)
-    def delete(self, connection, params):
-        self._connectionModel.remove(connection)
+    def delete(self, annotation_connection, params):
+        self._connectionModel.remove(annotation_connection)
 
     @describeRoute(Description("Update an existing connection")
                    .param('id', 'The ID of the connection.', paramType='path')
