e696d6644e00ace293d397ba35c044e10d6bbb9d
Adrien Boucaud <adrien.bk@free.fr>
2022-01-14 13:12:57 +0100

    Add annotation worker compute endpoint

--- devops/girder/plugins/AnnotationPlugin/upenncontrast_annotation/server/helpers/tasks.py
+++ devops/girder/plugins/AnnotationPlugin/upenncontrast_annotation/server/helpers/tasks.py
@@ -2,9 +2,9 @@ from girder_worker.docker.tasks import docker_run
 
 from girder.api.rest import getCurrentToken
 
-
 import datetime
 import json
+import re
 
 # TODO: security: disable pickle, never pull images ?
 # accept_content = ['json', 'yaml', 'girder_io']
@@ -15,22 +15,21 @@ import json
 # )
 
 
-def runComputeJob(property, datasetId, params):
-    image = property['image']
-
+def runComputeJob(image, datasetId, params):
+    name = params.get('name', 'unknown')
+    # Make sure name is a valid name for a docker container
+    name = ''.join(re.findall('[a-zA-Z0-9_.-]', name))
     params = json.dumps(params)
-
     job = docker_run.apply_async(
         (image,),
         kwargs={
             'pull_image': False,
             'container_args': ['--datasetId', datasetId, '--apiUrl', "http://localhost:8080/api/v1", '--token', getCurrentToken()['_id'], '--parameters', params],
             'remove_container': True,
-            'name': "{}_{}_{}".format(property['name'], datasetId, datetime.datetime.now().timestamp()),
+            'name': "{}_{}_{}".format(name, datasetId, datetime.datetime.now().timestamp()),
             # TODO: figure out network configuration and api url discovery
             'network_mode': 'host'
             # 'girder_result_hooks': [testHook]
         }
     ).job,
-
     return job
