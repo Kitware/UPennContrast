5e8ffe56f668d859888ec344c42491bc56c05715
Thibault Bruyère <thibault.bruyere@kitware.com>
2022-10-05 18:06:30 +0200

    Remove menu from the left panel

--- src/layout/Menu.vue
+++ src/layout/Menu.vue
@@ -1,13 +1,5 @@
 <template>
   <v-list dense>
-    <v-list-item to="/">
-      <v-list-item-action>
-        <v-icon>mdi-exit-to-app</v-icon>
-      </v-list-item-action>
-      <v-list-item-content>
-        <v-list-item-title>Home</v-list-item-title>
-      </v-list-item-content>
-    </v-list-item>
     <v-list-item :to="{ name: 'newdataset' }" v-if="store.isLoggedIn">
       <v-list-item-action>
         <v-icon>mdi-plus-circle</v-icon>
