ac6b7898d8dd53c607933a3c44fe9101fbf5537b
Thibault Bruyère <thibault.bruyere@kitware.com>
2022-10-26 14:26:58 +0200

    Add feature to add tags to selected annotations

--- src/components/AnnotationBrowser/AnnotationActions.vue
+++ src/components/AnnotationBrowser/AnnotationActions.vue
@@ -7,7 +7,29 @@
       <v-container>
         <v-row
           ><v-col><v-btn @click="deleteSelected">Delete Selected</v-btn></v-col>
-          <v-col><v-btn @click="tagSelected">Tag Selected</v-btn></v-col>
+          <v-col>
+            <v-dialog v-model="tagSelectedDialog" width="50%">
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn v-bind="attrs" v-on="on">Tag Selected</v-btn>
+              </template>
+              <v-card>
+                <v-card-title>
+                  Add tags to selected annotations
+                </v-card-title>
+                <tag-picker class="ma-4 pa-4" v-model="tagsToAdd"></tag-picker>
+                <v-divider></v-divider>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="warning" @click="tagsToAdd = []">
+                    Clear
+                  </v-btn>
+                  <v-btn color="primary" @click="tagSelected">
+                    Submit
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </v-col>
           <v-col
             ><v-btn @click="connectSelected">Connect Selected</v-btn></v-col
           ></v-row
@@ -20,9 +42,10 @@
 <script lang="ts">
 import { Vue, Component } from "vue-property-decorator";
 import annotationStore from "@/store/annotation";
+import TagPicker from "@/components/TagPicker.vue";
 
 @Component({
-  components: {}
+  components: { TagPicker }
 })
 export default class AnnotationActions extends Vue {
   readonly annotationStore = annotationStore;
@@ -42,8 +65,12 @@ export default class AnnotationActions extends Vue {
     this.annotationStore.deactivateSelectedAnnotations();
   }
 
+  tagSelectedDialog: boolean = false;
+  tagsToAdd: string[] = [];
   tagSelected() {
-    // TODO:
+    this.annotationStore.tagSelectedAnnotations(this.tagsToAdd);
+    this.tagSelectedDialog = false;
+    this.tagsToAdd = [];
   }
 
   connectSelected() {
