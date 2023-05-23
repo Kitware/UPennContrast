ee82fb479775e2a83197fb9b787f6f7feccb96b4
Thibault Bruyère <thibault.bruyere@kitware.com>
2023-05-23 11:59:16 +0200

    Fix worker interface values
    Fix select on worker interface deselecting tool
    Fix default values on mounted
    Fix sliders step

--- src/components/WorkerInterfaceValues.vue
+++ src/components/WorkerInterfaceValues.vue
@@ -1,61 +1,70 @@
 <template>
   <v-container class="pa-0 ma-0">
-    <v-row
-      v-for="[id, item] in Object.entries(workerInterface)"
-      :key="id"
-      class="pa-0 ma-0"
-    >
-      <v-col class="pa-0 ma-0" cols="4">
-        <v-subheader>
-          {{ id }}
-        </v-subheader>
-      </v-col>
-      <v-col class="pa-0 ma-0">
-        <v-slider
-          v-if="item.type === 'number'"
-          :max="item.max"
-          :min="item.min"
-          v-model="interfaceValues[id]"
-          :step="((item.max || 0) - (item.min || 0)) / 100.0"
-          class="align-center"
-        >
-          <template v-slot:append>
-            <v-text-field
-              v-model="interfaceValues[id]"
-              type="number"
-              style="width: 60px"
-              class="mt-0 pt-0"
-            >
-            </v-text-field>
-          </template>
-        </v-slider>
-        <v-text-field
-          v-if="item.type === 'text'"
-          v-model="interfaceValues[id]"
-          dense
-        ></v-text-field>
-        <tag-picker
-          v-if="item.type === 'tags'"
-          v-model="interfaceValues[id]"
-        ></tag-picker>
-        <layer-select
-          :clearable="!item.required"
-          v-if="item.type === 'layer'"
-          v-model="interfaceValues[id]"
-        ></layer-select>
-        <v-select
-          :clearable="!item.required"
-          v-if="item.type === 'select'"
-          v-model="interfaceValues[id]"
-          :items="item.items"
-        ></v-select>
-        <channel-select
-          :clearable="!item.required"
-          v-if="item.type === 'channel'"
-          v-model="interfaceValues[id]"
-        ></channel-select>
-      </v-col>
-    </v-row>
+    <!--
+      Dummy item group to prevent "change" events to be registered by a parent item group
+      See: https://github.com/Kitware/UPennContrast/pull/391#issuecomment-1557606390
+    -->
+    <v-list-item-group>
+      <v-row
+        v-for="[id, item] in Object.entries(workerInterface)"
+        :key="id"
+        class="pa-0 ma-0"
+      >
+        <v-col class="pa-0 ma-0" cols="4">
+          <v-subheader>
+            {{ id }}
+          </v-subheader>
+        </v-col>
+        <v-col class="pa-0 ma-0">
+          <v-slider
+            v-if="item.type === 'number'"
+            v-model="interfaceValues[id]"
+            :max="item.max"
+            :min="item.min"
+            :step="((item.max || 0) - (item.min || 0)) / 100.0"
+            class="align-center"
+          >
+            <template v-slot:append>
+              <v-text-field
+                v-model="interfaceValues[id]"
+                type="number"
+                :max="item.max"
+                :min="item.min"
+                :step="((item.max || 0) - (item.min || 0)) / 100.0"
+                style="width: 60px"
+                class="mt-0 pt-0"
+              >
+              </v-text-field>
+            </template>
+          </v-slider>
+          <v-text-field
+            v-if="item.type === 'text'"
+            v-model="interfaceValues[id]"
+            dense
+          ></v-text-field>
+          <tag-picker
+            v-if="item.type === 'tags'"
+            v-model="interfaceValues[id]"
+          ></tag-picker>
+          <layer-select
+            :clearable="!item.required"
+            v-if="item.type === 'layer'"
+            v-model="interfaceValues[id]"
+          ></layer-select>
+          <v-select
+            :clearable="!item.required"
+            v-if="item.type === 'select'"
+            v-model="interfaceValues[id]"
+            :items="item.items"
+          ></v-select>
+          <channel-select
+            :clearable="!item.required"
+            v-if="item.type === 'channel'"
+            v-model="interfaceValues[id]"
+          ></channel-select>
+        </v-col>
+      </v-row>
+    </v-list-item-group>
   </v-container>
 </template>
 
@@ -101,6 +110,10 @@ export default class WorkerInterfaceValues extends Vue {
       case "channel":
         return 0;
     }
+  }
+
+  mounted() {
+    this.resetValues();
   }
 
   @Watch("workerInterface")
