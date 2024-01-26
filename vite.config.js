01d6f9f3e0b9c06e8a0c98c34333974fe77530ee
Thibault Bruyère <thibault.bruyere@kitware.com>
2024-01-26 10:45:18 +0100

    Move from CJS to ESM module, replace copy webpack

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from "vite";
+import { defineConfig, normalizePath } from "vite";
 import vue from "@vitejs/plugin-vue2";
 import { VuetifyResolver } from "unplugin-vue-components/resolvers";
 import Components from "unplugin-vue-components/vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+import path from "node:path";
+
+function joinAndNormalizePath(...paths) {
+  return normalizePath(path.join(...paths));
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +17,66 @@ export default defineConfig({
     vue(),
     Components({
       resolvers: [VuetifyResolver()],
+    }),
+    viteStaticCopy({
+      targets: [
+        {
+          src: joinAndNormalizePath(
+            __dirname,
+            "node_modules",
+            "itk",
+            "WebWorkers",
+          ),
+          dest: joinAndNormalizePath("itk"),
+        },
+        {
+          src: joinAndNormalizePath(
+            __dirname,
+            "node_modules",
+            "itk",
+            "ImageIOs",
+          ),
+          dest: joinAndNormalizePath("itk"),
+        },
+        {
+          src: joinAndNormalizePath(
+            __dirname,
+            "node_modules",
+            "itk",
+            "PolyDataIOs",
+          ),
+          dest: joinAndNormalizePath("itk"),
+        },
+        {
+          src: joinAndNormalizePath(
+            __dirname,
+            "node_modules",
+            "itk",
+            "MeshIOs",
+          ),
+          dest: joinAndNormalizePath("itk"),
+        },
+        {
+          src: joinAndNormalizePath(
+            __dirname,
+            "itk",
+            "web-build",
+            "**",
+            "*.{js,wasm}",
+          ),
+          dest: joinAndNormalizePath("itk", "Pipelines"),
+        },
+        {
+          src: joinAndNormalizePath(
+            __dirname,
+            "node_modules",
+            "onnxruntime-web",
+            "dist",
+            "*.wasm",
+          ),
+          dest: joinAndNormalizePath("js"),
+        },
+      ],
     }),
   ],
   // css: {
