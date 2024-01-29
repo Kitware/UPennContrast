9ddbf92212d21e9a458393a19b3a998d49d3f42a
Thibault Bruyère <thibault.bruyere@kitware.com>
2024-01-29 16:50:18 +0100

    Fix an eslint rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,8 +7,8 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    // Required to access v-data-table scoped slots, see https://vuetifyjs.com/en/components/data-tables/#slots
-    "vue/valid-v-slot": "off",
+    // Required to access v-data-table scoped slots, see https://v2.vuetifyjs.com/en/components/data-tables/#slots
+    "vue/valid-v-slot": ["error", { allowModifiers: true }],
   },
   parserOptions: {
     parser: "@typescript-eslint/parser",
