89d6177ff5d7f2438abf68df421c0e28f7c4e8e3
arjunrajlab <arjunrajlab@gmail.com>
2024-09-15 10:54:26 -0400

    Update UserMenu.vue

--- src/layout/UserMenu.vue
+++ src/layout/UserMenu.vue
@@ -6,65 +6,134 @@
       </template>
       <v-container class="pa-0">
         <v-card class="pa-6">
-          <div class="text-center mb-8">
-            <v-img
-              src="/img/icons/NimbusImageIcon.png"
-              max-height="80"
-              contain
-              class="mb-2"
-            />
-            <h2 class="text-h5 font-weight-bold mb-2">
-              Welcome to NimbusImage!
-            </h2>
-            <p class="text-subtitle-1">
-              A cloud-based image analysis platform from the Raj Lab at the
-              University of Pennsylvania and Kitware
-            </p>
-          </div>
-          <v-form @submit.prevent="login" class="my-8">
-            <v-text-field
-              v-model="domain"
-              name="domain"
-              label="Girder Domain"
-              required
-              prepend-icon="mdi-domain"
-            />
-            <v-text-field
-              v-model="username"
-              name="username"
-              label="Username or e-mail"
-              required
-              prepend-icon="mdi-account"
-            />
-            <v-text-field
-              v-model="password"
-              name="password"
-              type="password"
-              label="Password"
-              prepend-icon="mdi-lock"
-            />
-            <v-card-actions class="button-bar">
-              <v-btn type="submit" color="primary">Login</v-btn>
-            </v-card-actions>
-          </v-form>
-          <v-alert :value="!!error" color="error">{{ error }}</v-alert>
-          <div class="text-center mt-4">
-            <a
-              href="https://arjun-raj-lab.gitbook.io/nimbusimage"
-              target="_blank"
-              class="link"
-            >
-              More information
-            </a>
-            <br />
-            <a
-              href="https://arjun-raj-lab.gitbook.io/nimbusimage/quick-start#sign-up"
-              target="_blank"
-              class="link"
-            >
-              Sign up
-            </a>
-          </div>
+          <template v-if="!signUpMode">
+            <div class="text-center mb-8">
+              <v-img
+                src="/img/icons/NimbusImageIcon.png"
+                max-height="80"
+                contain
+                class="mb-2"
+              />
+              <h2 class="text-h5 font-weight-bold mb-2">
+                Welcome to NimbusImage!
+              </h2>
+              <p class="text-subtitle-1">
+                A cloud-based image analysis platform from the Raj Lab at the
+                University of Pennsylvania and Kitware
+              </p>
+            </div>
+            <v-alert :value="!!successMessage" type="success">{{
+              successMessage
+            }}</v-alert>
+            <v-form @submit.prevent="login" class="my-8">
+              <v-text-field
+                v-if="!isGirderUrlSet"
+                v-model="domain"
+                name="domain"
+                label="Girder Domain"
+                required
+                prepend-icon="mdi-domain"
+              />
+              <v-text-field
+                v-model="username"
+                name="username"
+                label="Username or e-mail"
+                required
+                prepend-icon="mdi-account"
+              />
+              <v-text-field
+                v-model="password"
+                name="password"
+                type="password"
+                label="Password"
+                prepend-icon="mdi-lock"
+              />
+              <v-card-actions class="button-bar">
+                <v-btn type="submit" color="primary">Login</v-btn>
+              </v-card-actions>
+            </v-form>
+            <v-alert :value="!!error" color="error">{{ error }}</v-alert>
+            <div class="text-center mt-4">
+              <a
+                href="https://arjun-raj-lab.gitbook.io/nimbusimage"
+                target="_blank"
+                class="link"
+              >
+                More information
+              </a>
+              <br />
+              <v-btn text class="link" @click="switchToSignUp">Sign up</v-btn>
+            </div>
+          </template>
+          <template v-else>
+            <div class="text-center mb-8">
+              <v-img
+                src="/img/icons/NimbusImageIcon.png"
+                max-height="80"
+                contain
+                class="mb-2"
+              />
+              <h2 class="text-h5 font-weight-bold mb-2">
+                Sign up for NimbusImage!
+              </h2>
+              <p class="text-subtitle-1">
+                Create a new account to get started.
+              </p>
+            </div>
+            <v-form @submit.prevent="signUp" class="my-8">
+              <v-text-field
+                v-model="signupUsername"
+                name="username"
+                label="Username"
+                required
+                prepend-icon="mdi-account"
+              />
+              <v-text-field
+                v-model="signupEmail"
+                name="email"
+                label="Email"
+                required
+                prepend-icon="mdi-email"
+              />
+              <v-text-field
+                v-model="signupFirstName"
+                name="firstName"
+                label="First Name"
+                required
+                prepend-icon="mdi-account"
+              />
+              <v-text-field
+                v-model="signupLastName"
+                name="lastName"
+                label="Last Name"
+                required
+                prepend-icon="mdi-account"
+              />
+              <v-text-field
+                v-model="signupPassword"
+                name="password"
+                type="password"
+                label="Password"
+                required
+                prepend-icon="mdi-lock"
+              />
+              <v-card-actions class="button-bar">
+                <v-btn type="submit" color="primary">Sign up</v-btn>
+                <v-spacer></v-spacer>
+                <v-btn text @click="switchToLogin">Back to login</v-btn>
+              </v-card-actions>
+            </v-form>
+            <v-alert :value="!!error" color="error">{{ error }}</v-alert>
+            <div class="text-center mt-4">
+              <a
+                href="https://arjun-raj-lab.gitbook.io/nimbusimage"
+                target="_blank"
+                class="link"
+              >
+                More information
+              </a>
+            </div>
+          </template>
         </v-card>
       </v-container>
     </v-dialog>
@@ -107,6 +176,18 @@ export default class UserMenu extends Vue {
   password = import.meta.env.VITE_DEFAULT_PASSWORD || "";
 
   error = "";
+  successMessage = "";
+
+  signUpMode: boolean = false;
+  signupUsername: string = "";
+  signupEmail: string = "";
+  signupFirstName: string = "";
+  signupLastName: string = "";
+  signupPassword: string = "";
+
+  get isGirderUrlSet(): boolean {
+    return !!import.meta.env.VITE_GIRDER_URL;
+  }
 
   async mounted() {
     this.loggedInOrOut();
@@ -122,6 +203,7 @@ export default class UserMenu extends Vue {
 
   async login() {
     this.error = "";
+    this.successMessage = "";
     try {
       const result = await store.login({
         domain: this.domain,
@@ -136,6 +218,74 @@ export default class UserMenu extends Vue {
     } finally {
       this.password = "";
     }
+  }
+
+  async signUp() {
+    this.error = "";
+    try {
+      // Simulate server response
+      if (this.signupUsername === "existing") {
+        throw {
+          field: "login",
+          message: "That login is already registered.",
+          type: "validation",
+        };
+      }
+      if (this.signupEmail === "existing@example.com") {
+        throw {
+          field: "email",
+          message: "That email is already registered.",
+          type: "validation",
+        };
+      }
+      // Simulate success
+      const response = {
+        _accessLevel: 2,
+        _id: "66e6b9682410668ab75ceccf",
+        _modelType: "user",
+        admin: false,
+        created: new Date().toISOString(),
+        email: this.signupEmail,
+        emailVerified: false,
+        firstName: this.signupFirstName,
+        groupInvites: [],
+        groups: [],
+        lastName: this.signupLastName,
+        login: this.signupUsername,
+        otp: false,
+        public: false,
+        size: 0,
+        status: "enabled",
+      };
+      // On success, return to login page
+      this.signUpMode = false;
+      this.signupUsername = "";
+      this.signupEmail = "";
+      this.signupFirstName = "";
+      this.signupLastName = "";
+      this.signupPassword = "";
+      this.successMessage =
+        "Sign-up successful! Please verify your email before logging in.";
+    } catch (err) {
+      // Handle error
+      if (err instanceof Error) {
+        this.error = err.message;
+      } else {
+        this.error = "An error occurred during sign-up.";
+      }
+    }
+  }
+
+  switchToSignUp() {
+    this.signUpMode = true;
+    this.error = "";
+    this.successMessage = "";
+  }
+
+  switchToLogin() {
+    this.signUpMode = false;
+    this.error = "";
+    this.successMessage = "";
   }
 }
 </script>
