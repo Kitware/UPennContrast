09fc2b4be5935ff451396a34a920c562584d1f9f
Arjun Raj <arjunrajlab@gmail.com>
2024-10-05 05:44:22 -0400

    Fix naming convention

--- src/components/AnnotationBrowser/AnnotationProperties/PropertyCreation.vue
+++ src/components/AnnotationBrowser/AnnotationProperties/PropertyCreation.vue
@@ -28,10 +28,7 @@
           </v-col>
         </v-row>
       </v-container>
-      <v-container
-        class="elevation-3 mt-4"
-        v-if="filteringShape !== null && filteringTags.length > 0"
-      >
+      <v-container class="elevation-3 mt-4" v-if="filteringShape !== null">
         <div class="pb-4 subtitle-1">Measure this property:</div>
         <v-row>
           <v-col>
@@ -68,9 +65,9 @@
       <div class="button-bar">
         <v-spacer></v-spacer>
         <v-btn class="mr-4" color="primary" @click="createProperty">
-          SUBMIT
+          Create Property
         </v-btn>
-        <v-btn class="mr-4" color="warning" @click="reset">CANCEL</v-btn>
+        <v-btn class="mr-4" color="warning" @click="reset">Cancel</v-btn>
       </div>
     </v-card-text>
   </v-card>
@@ -161,7 +158,11 @@ export default class PropertyCreation extends Vue {
     if (this.filteringTags.length) {
       nameList.push(this.filteringTags.join(", "));
     } else {
-      nameList.push("No tag");
+      if (this.areTagsExclusive) {
+        nameList.push("No tag");
+      } else {
+        nameList.push("All");
+      }
     }
     if (this.dockerImage) {
       const imageInterfaceName =
@@ -238,11 +239,7 @@ export default class PropertyCreation extends Vue {
   }
 
   createProperty() {
-    if (
-      !this.dockerImage ||
-      !this.filteringShape ||
-      !this.filteringTags.length
-    ) {
+    if (!this.dockerImage || !this.filteringShape) {
       return;
     }
     this.propertyStore
