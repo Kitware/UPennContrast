b336d9fd9dada6f3cc8caaf5ae1dc26eef2b1645
Arjun Raj <arjunrajlab@gmail.com>
2024-10-24 10:11:32 -0400

    Added an "enabled" prop and formatted with HTML/page breaks.

--- src/components/NimbusTooltip.vue
+++ src/components/NimbusTooltip.vue
@@ -1,5 +1,6 @@
 <template>
   <v-tooltip
+    v-if="enabled"
     :top="position === 'top'"
     :right="position === 'right'"
     :bottom="position === 'bottom'"
@@ -7,7 +8,7 @@
     :activator="activator"
     :open-delay="open_delay"
   >
-    <span>{{ content }}</span>
+    <span v-html="formattedTooltip(content)"></span>
   </v-tooltip>
 </template>
 
@@ -21,6 +22,13 @@ export default Vue.extend({
     content: { type: String, required: true },
     position: { type: String, required: false, default: "bottom" },
     open_delay: { type: Number, default: 0 },
+    enabled: { type: Boolean, default: true },
+  },
+
+  methods: {
+    formattedTooltip(text: string): string {
+      return text.replace(/\n/g, "<br>");
+    },
   },
 });
 </script>
