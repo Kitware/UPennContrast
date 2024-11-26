255ec16457d63e2dd2b2d7812612d4291471e409
arjunrajlab <arjunrajlab@gmail.com>
2024-11-26 08:06:26 -0500

    Lint

--- src/components/AnnotationViewer.vue
+++ src/components/AnnotationViewer.vue
@@ -1237,8 +1237,8 @@ export default class AnnotationViewer extends Vue {
       .data(textPoints)
       .position((d: IGeoJSPosition) => d)
       .style({
-        text: (d: IGeoJSPosition, i: number) => textLabels[i],
-        fontSize: (d: IGeoJSPosition, i: number) =>
+        text: (_: IGeoJSPosition, i: number) => textLabels[i],
+        fontSize: (_: IGeoJSPosition, i: number) =>
           textStyles[i].fontSize || "12px",
         fontFamily: "sans-serif",
         textAlign: "center",
